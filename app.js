0f0df97d1784a5dc9d17a012af6fca1d112c6031
DimCheck <x.chekushkin.dima@gmail.com>
2023-07-10 18:17:24 +0400

    Сделал калькулятор, оптимизировал код

--- app.js
+++ app.js
@@ -65,23 +65,31 @@ minusBtn.onclick = function () {
     action = "-"
 }
 
-submitBtn.onclick = function ( ) {
-
-    if (action == "+" ) {
-    const sum = Number(input1.value) + Number (input2.value)
-    if (sum < 0 ) {
+function printResult (result) {
+    if ( result < 0 ) {
         resultElement.style.color = "red"
     } else {
         resultElement.style.color = "green"
     }
-    resultElement.textContent = sum 
-    } else if (action == "-" ) {
-        const sum = Number(input1.value) - Number (input2.value)
-        if (sum < 0 ) {
-            resultElement.style.color = "red"
-        } else {
-            resultElement.style.color = "green"
-        }
-        resultElement.textContent = sum 
+    resultElement.textContent = result 
+}
+
+function computeNumbersWithAction (inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1, value)
+    const num2 = Number(inp2, value)
+    if (actionSymbol == "+") {
+        return num1 + num2
+    } else if (actionSymbol == "-") {
+        return num1 - num2
     }
+}
+
+submitBtn.onclick = function ( ) {
+    if (action == "+" ) {
+    const sum = Number(input1.value) + Number (input2.value)
+    printResult (sum)
+    } else if (action == "-" ) {
+        const sum = Number(input1.value) - Number(input2.value)
+        printResult(sum)
+    } 
 }
