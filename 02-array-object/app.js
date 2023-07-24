9b7464dde91bcb27703f4feae86321957ea24788
DimCheck <x.chekushkin.dima@gmail.com>
2023-07-24 14:09:35 +0400

    Начился добавлять заметки

--- 02-array-object/app.js
+++ 02-array-object/app.js
@@ -5,8 +5,45 @@ const inputElement = document.getElementById('title')
 const createBtn = document.getElementById('create')
 const ListElement = document.getElementById('list')
 
-console.log(inputElement.value)
+// console.log(inputElement.value)
+
+const notes = ['Записать блок про массивы', 'рассказать теорию обьектов']
+
+function render() {
+    ListElement.insertAdjacentHTML(
+        'beforeend', 
+        getNoteTempLate(notes[0])
+    )
+    ListElement.insertAdjacentHTML(
+        'beforeend', 
+        getNoteTempLate(notes[1])
+    )
+}
+
+render()
 
 createBtn.onclick = function () {
-    ListElement.innerHTML = ":D"
+if (inputElement.value.length === 0) {
+    return 
+}
+     //ListElement.innerHTML = 
+    ListElement.insertAdjacentHTML(
+        'beforeend',
+        getNoteTempLate(inputElement.value)
+    )
+    inputElement.value = ''
+}
+
+function getNoteTempLate(title) {
+    return `
+        <li
+            class="list-group-item d-flex justify-content-between align-items-center"
+        >
+            <span>${title}</span>
+            <span>
+                <span class="btn btn-small btn-success">&check;</span>
+                <span class="btn btn-small btn-danger">&times;</span>
+            </span>
+        </li>
+    `
 }
